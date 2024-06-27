d4b916c091fefaf34e39839167c61bb972af56bd
Pabin R Luitel <pabin2050@gmail.com>
2024-06-27 21:18:52 +0545

    add type for helloWorld param and comment on helloWorld method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { IUser } from './schemas/user.schema';
 
 @Controller('users')
 @UseGuards(AuthGuard())
@@ -8,7 +10,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  helloWorld(@Req() req) {
-    return this.usersService.helloWorld(req.user);
+  helloWorld(@Req() req: Request) {
+    return this.usersService.helloWorld(req.user as IUser);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,13 @@ import { IUser } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
-  async helloWorld(user: IUser): Promise<string> {
+  /**
+   * Demo method to show auth validation with AuthGuard.
+   *
+   * @param {IUser} user - The user object containing user information.
+   * @returns {string} A greeting message indicating the user is authenticated.
+   */
+  helloWorld(user: IUser): string {
     return `Hi, ${user.firstName} is authenticated !!`;
   }
 }
