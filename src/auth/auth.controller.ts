91670bba3a5bd91c2e8354e6129970802144a316
Pabin R Luitel <pabin2050@gmail.com>
2024-06-27 13:16:51 +0545

    Add users module and auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+
+@Controller({ path: 'auth', version: '1' })
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  userRegistration(@Body() createUserDto: CreateUserDto) {
+    return this.authService.userRegistration(createUserDto);
+  }
+
+  @Post('login')
+  userLogin(@Body() userLoginDto: UserLoginDto) {
+    return this.authService.userLogin(userLoginDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+
+@Injectable()
+export class AuthService {
+  userRegistration(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  userLogin(userLoginDto: UserLoginDto) {
+    return `This action returns all users`;
+  }
+}
