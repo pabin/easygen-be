7d01f68547d15ad43bcba286df4c276300a77422
Pabin R Luitel <pabin2050@gmail.com>
2024-06-27 15:38:51 +0545

    setup mongoose and remove typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 27017,
-      database: process.env.DATABASE_NAME,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: false,
-    }),
+    MongooseModule.forRoot(process.env.DATABASE),
     UsersModule,
     AuthModule,
   ],
